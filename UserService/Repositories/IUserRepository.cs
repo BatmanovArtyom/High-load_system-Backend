18db8012fc3ab18fd813b4b0adfe6e0288829b49
ArtBat <artyom.batman.batmanov@yandex.ru>
2024-10-11 18:39:53 +0300

    Create the Repository interface

--- UserService/Repositories/IUserRepository.cs
+++ UserService/Repositories/IUserRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace UserService.Repositories;
+﻿using UserService.Models;
+
+namespace UserService.Repositories;
 
 public interface IUserRepository
 {
-    
+    Task<bool> CreateUser(User user);
+    Task<User> GetUserById(int id);
+    Task<User> GetUserByName(string name, string surname);
+    Task<bool> UpdateUser(User user);
+    Task<bool> DeleteUser(int id);
+
 }

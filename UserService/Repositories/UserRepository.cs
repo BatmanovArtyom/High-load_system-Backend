d7d7aa8a59370858ca77fb6182c988ed65f6c073
ArtBat <artyom.batman.batmanov@yandex.ru>
2024-10-16 20:04:17 +0300

    fix: update Repository and IRepository: use DynamicParameters and CancellationToken

--- UserService/Repositories/UserRepository.cs
+++ UserService/Repositories/UserRepository.cs
@@ -13,64 +13,67 @@ public class UserRepository : IUserRepository
         _dbConnection = dbConnection;
     }
 
-    public async Task<bool> CreateUser(User user)
+    public async Task<bool> CreateUser(User user, CancellationToken cancellationToken)
     {
-        var userId = await _dbConnection.ExecuteScalarAsync<int>(
-            "SELECT create_user (@Login, @Password, @Name, @Surname, @Age)", new
-            {
-                user.Login,
-                user.Password,
-                user.Name,
-                user.Surname,
-                user.Age
-            });
-
-        if (userId > 0)
-        {
-            return true;
-        }
+        var parameters = new DynamicParameters();
+        parameters.Add("@Login", user.Login);
+        parameters.Add("@Password", user.Password);
+        parameters.Add("@Name", user.Name);
+        parameters.Add("@Surname", user.Surname);
+        parameters.Add("@Age", user.Age);
         
-        return false;
+        
+        var command = new CommandDefinition("SELECT create_user (@Login, @Password, @Name, @Surname, @Age)", parameters, cancellationToken: cancellationToken);
+        var userId = await _dbConnection.QuerySingleAsync<int>(command);
+
+        return userId != 0;
     }
     
-    public async Task<User> GetUserById(int id)
+    public async Task<User> GetUserById(int id, CancellationToken cancellationToken)
     {
-        if (id < 0)
-        {
-            throw new ArgumentException("ID должен быть больше нуля.", nameof(id));
-        }
-
-        var user = await _dbConnection.QuerySingleOrDefaultAsync<User>("SELECT * FROM get_user_by_id(@Id)", new { id = id });
+        var parameters = new DynamicParameters();
+        parameters.Add("@Id", id);
+        
+        var command = new CommandDefinition("SELECT * FROM get_user_by_id(@Id)", parameters, cancellationToken: cancellationToken);
+        var user = await _dbConnection.QuerySingleOrDefaultAsync<User>(command);
         
         return user;
     }
 
-    public async Task<User> GetUserByName(string name, string surname)
+    public async Task<User> GetUserByName(string name, string surname, CancellationToken cancellationToken)
     {
-        var user = await _dbConnection.QuerySingleOrDefaultAsync<User>( "SELECT * FROM get_user_by_name(@Name, @Surname)", new { Name = name, Surname = surname });
+        var parameters = new DynamicParameters();
+        parameters.Add("@Name", name);
+        parameters.Add("@Surname", surname);
+        
+        var command = new CommandDefinition("SELECT * FROM get_user_by_name(@Name)", parameters, cancellationToken: cancellationToken);
+        var user = await _dbConnection.QuerySingleOrDefaultAsync<User>(command);
         
         return user;
     }
     
-    public async Task<bool> UpdateUser(User user)
+    public async Task<bool> UpdateUser(User user, CancellationToken cancellationToken)
     {
-        var success = await _dbConnection.ExecuteScalarAsync<bool>(
-            "SELECT update_user(@Id, @Name, @Surname, @Age)",
-            new
-            {
-                user.Id,
-                user.Name,
-                user.Surname,
-                user.Age
-            });
+        var parameters = new DynamicParameters();
+        parameters.Add("@Id", user.Id);
+        parameters.Add("@Name", user.Name);
+        parameters.Add("@Surname", user.Surname);
+        parameters.Add("@Age", user.Age);
+        
+        
+        var command = new CommandDefinition("SELECT update_user(@Id, @Name, @Surname, @Age)", parameters, cancellationToken: cancellationToken);
+        var success = await _dbConnection.ExecuteScalarAsync<bool>(command);
 
         return success;
     }
     
-    public async Task<bool> DeleteUser(int id)
+    public async Task<bool> DeleteUser(int id, CancellationToken cancellationToken)
     {
-        var success = await _dbConnection.ExecuteScalarAsync<bool>(
-            "SELECT delete_user(@Id)", new { Id = id });
+        var parameters = new DynamicParameters();
+        parameters.Add("@Id", id);
+        
+        var command = new CommandDefinition("SELECT delete_user(@Id)", parameters, cancellationToken: cancellationToken);
+        var success = await _dbConnection.ExecuteScalarAsync<bool>(command);
 
         return success;
     }

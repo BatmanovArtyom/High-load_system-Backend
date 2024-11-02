8ae9c0f9b15fc1582854ef877bfb84d4f3461bbf
Olga <percent000111@gmail.com>
2024-11-02 17:06:15 +0300

    RateLimit Repo

---
+++ RateLimiter.Writer/Models/DbModels/RateLimitDbModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RateLimiter.Writer.Models.DbModels;
+
+public class RateLimitDbModel
+{
+    public int Id { get; set; }
+    public string Route { get; set; }
+    public int RequestsPerMinute { get; set; }
+}
---
+++ RateLimiter.Writer/Repository/IRateLimitRepository.cs
@@ -0,0 +1,12 @@
+﻿using RateLimiter.Writer.Models.DbModels;
+using RateLimiter.Writer.Models.DomainModels;
+
+namespace RateLimiter.Writer.Repository;
+
+public interface IRateLimitRepository
+{
+    Task<bool> CreateAsync(RateLimit rateLimit, CancellationToken cancellationToken);
+    Task<RateLimit> GetByRouteAsync(string route, CancellationToken cancellationToken);
+    Task<bool> UpdateAsync(RateLimit rateLimit,CancellationToken cancellationToken);
+    Task<bool> DeleteAsync(string route,CancellationToken cancellationToken);
+}

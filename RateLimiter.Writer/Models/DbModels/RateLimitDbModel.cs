404220e5bb948bb2eeca2ee39a7765aba2064f8a
ArtBat <artyom.batman.batmanov@yandex.ru>
2024-11-05 15:49:01 +0300

    update dbmodel

--- RateLimiter.Writer/Models/DbModels/RateLimitDbModel.cs
+++ RateLimiter.Writer/Models/DbModels/RateLimitDbModel.cs
@@ -1,8 +1,17 @@
-﻿namespace RateLimiter.Writer.Models.DbModels;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace RateLimiter.Writer.Models.DbModels;
 
 public class RateLimitDbModel
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public int Id { get; set; }
+    
+    [BsonElement("route")]
     public string Route { get; set; }
+    
+    [BsonElement("requests_per_minute")]
     public int RequestsPerMinute { get; set; }
 }

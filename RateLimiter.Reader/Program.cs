478c7f1c4a6835477394f36d2a91247237caefa1
ArtBat <artyom.batman.batmanov@yandex.ru>
2024-11-12 18:19:51 +0300

    Update the Program and json files

--- RateLimiter.Reader/Program.cs
+++ RateLimiter.Reader/Program.cs
@@ -1,13 +1,26 @@
 using RateLimiter.Reader.Controllers;
+using RateLimiter.Reader.Database;
+using RateLimiter.Reader.DomainService;
+using RateLimiter.Reader.Repository;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("ConnectionStrings"));
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+builder.Services.AddSingleton<IRateLimitRepository, RateLimitRepository>();
+builder.Services.AddSingleton<ReaderService>();
+builder.Services.AddSingleton<DatabaseInitializer>();
+
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.MapGrpcService<ReaderController>();
+
+app.MapGet("/", () => "gRPC service is running. Use a gRPC client to connect.");
 
 await app.RunAsync("http://*:5000");

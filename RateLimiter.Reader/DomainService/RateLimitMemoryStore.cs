93fa3abf861e93fd845cbc7e841a8abbb52ba788
ArtBat <artyom.batman.batmanov@yandex.ru>
2024-11-23 16:40:32 +0300

    add delete

--- RateLimiter.Reader/DomainService/RateLimitMemoryStore.cs
+++ RateLimiter.Reader/DomainService/RateLimitMemoryStore.cs
@@ -2,30 +2,21 @@ using System.Collections.Concurrent;
 using Microsoft.Extensions.Logging;
 using MongoDB.Bson;
 using RateLimiter.Reader.Models.DbModels;
+using RateLimiter.Reader.Models.DomainModels;
 
 namespace RateLimiter.Reader.DomainService;
 
 public class RateLimitMemoryStore
 {
-    private readonly ConcurrentDictionary<ObjectId, ReaderDbModel> _rateLimits = new();
+    private readonly ConcurrentDictionary<ObjectId, RateLimits> _rateLimits = new();
     private readonly ILogger<RateLimitMemoryStore> _logger;
 
     public RateLimitMemoryStore(ILogger<RateLimitMemoryStore> logger)
     {
         _logger = logger;
     }
-
-    public void AddOrUpdatBatch(IEnumerable<ReaderDbModel> rateLimits)
-    {
-        foreach (var rateLimit in rateLimits)
-        {
-            _rateLimits[rateLimit.Id] = rateLimit;
-            _logger.LogInformation("Added rate limit: Id={Id}, Route={Route}, RequestsPerMinute={RequestsPerMinute}",
-                rateLimit.Id, rateLimit.Route, rateLimit.RequestsPerMinute);
-        }
-    }
-
-    public void AddOrUpdateSingle(ReaderDbModel rateLimit)
+    
+    public void AddOrUpdateSingle(RateLimits rateLimit)
     {
         _rateLimits[rateLimit.Id] = rateLimit;
         _logger.LogInformation("Added rate limit: Id={Id}, Route={Route}, RequestsPerMinute={RequestsPerMinute}",
@@ -38,5 +29,11 @@ public class RateLimitMemoryStore
         {
             _logger.LogInformation($"Removed entry: {removed.Route}");
         }
+    }
+    
+    public ICollection<RateLimits> GetAllRateLimits()
+    {
+        _logger.LogInformation("Fetching all rate limits from memory.");
+        return _rateLimits.Values;
     }
 }
